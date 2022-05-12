b2267f491f3af2f79fef8d28b6a9ec87d20c3541
Ecemnur Naz <75392408+ecemnurnaz@users.noreply.github.com>
2022-05-12 21:54:43 +0300

    Delete EcemnurNazHw3 directory

--- EcemnurNazHw3/src/main/java/edu/sabanciuniv/mts/MTRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sabanciuniv.mts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MTRepository extends JpaRepository<Transfer, Integer> {
-
-}
